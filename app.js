ebdfd0606fc3de165ca739c4ce9b5560947170af
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2018-08-09 21:54:47 +0200

    delete innecesary files

--- app.js
+++ app.js
@@ -55,12 +55,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/auth', auth);
+app.use('/auth', auth);
 app.use('/api', phones);
-
-// app.use((req, res) => {
-//   res.sendfile(`${__dirname}/public/index.html`);
-// });
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
