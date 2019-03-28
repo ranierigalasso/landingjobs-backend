8c931700703d0a7b23343446cced2748319ad3b9
ranierigalasso <ranierigalasso@gmail.com>
2019-03-28 16:17:07 +0100

    ddf

--- bin/seed.js
+++ bin/seed.js
@@ -9,7 +9,7 @@ const politicians = [
   {name: 'Eleonor Jessin', joinDate:'1980-12-08'},
 ];
 
-mongoose.connect(proccess.env.DATABASE, { useNewUrlParser: true })
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true })
   .then(() => {
     console.log('connected to db');
     return Politician.create(politicians);
